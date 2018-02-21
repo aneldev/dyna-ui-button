e760899845ebbf639a4923c9b62b6b4fccbb534c
Dennis Teo <dynasdaq@gmail.com>
2018-02-21 07:14:11 +0100

    bootstrap update

--- tests/ui/index.spec.tsx
+++ tests/ui/index.spec.tsx
@@ -1,4 +1,5 @@
-import 'jsdom-global/register';
+declare let global: any, jasmine: any, describe: any, clearTest: any, it: any, expect: any;
+
 import { configure } from 'enzyme';
 import * as Adapter from 'enzyme-adapter-react-16';
 configure({ adapter: new Adapter() });
@@ -6,14 +7,14 @@ configure({ adapter: new Adapter() });
 import * as React from 'react';
 import * as enzyme from 'enzyme';
 
-import ComponentToTest from './../../src';
+import {DynaButton} from './../../src';
 
 describe('Home', () => {
   let wrapper;
 
   it('has expected content with deep render', () => {
     wrapper = enzyme.shallow(
-      <ComponentToTest caption="ok" onClick={() => console.log('I am clicked')}/>,
+      <DynaButton>hello</DynaButton>,
       {}
     );
 
